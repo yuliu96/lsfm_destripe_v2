f25387b3227f5c0a9c9462202de4594be53a1e57
Jianxu Chen <jxchen.ustc@gmail.com>
2024-12-11 19:38:42 +0100

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     url="https://github.com/peng-lab/lsfm_destripe",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="0.1.0",
     zip_safe=False,
 )
